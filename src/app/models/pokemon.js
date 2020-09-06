d0bfcf75dd72e9df13efce9de871677ff530ad63
cloudsystems22 <cloudsystems22@gmail.com>
2020-09-06 19:02:22 -0300

    Alteração na tabela pokemons

--- src/app/models/pokemon.js
+++ src/app/models/pokemon.js
@@ -6,10 +6,6 @@ const PokemonSchema = new mongoose.Schema({
         ref:'User',
         require: true
     },
-    number:{
-        type:String,
-        required: true,
-    },
     name: {
         type: String,
         unique: true,
@@ -19,72 +15,14 @@ const PokemonSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    about:{
-        type: String,
-        required: true,
-    },
     types:[{
         type:String
-    }],
-    resistant:[{
-        type:String
-    }],
-    weaknesses:[{
-        type:String,
     }],
     fastAttacks:[{
         name:{ type:String},
         type:{ type:String},
         damage:{ type:Number},
     }],
-    specialAttacks:[{
-        name:{ type:String},
-        type:{ type:String},
-        damage:{ type:Number},
-    }],
-    weight:{
-        type:Object({
-            minimun:String,
-            maximun:String,
-        })
-    },
-    height:{
-        type:Object({
-            minimun:String,
-            maximun:String,
-        })
-    },
-    buddyDistance:{
-        type:String,
-    },
-    baseStamina:{
-        type:String,
-    },
-    baseAttack:{
-        type:String,
-    },
-    baseDefense:{
-        type:String,
-    },
-    baseFlatRate:{
-        type:String,
-    },
-    nextEvolutionRequirements:{
-        type:Object({
-            amount:{type:String},
-            name:{type:String},
-        })
-    },
-    nextEvolutions:[{
-        number:{ type:String},
-        name:{ type:String},  
-    }],
-    maxCP:{
-        type:Number,
-    },
-    minCP:{
-        type:Number,
-    },
     createdAt:{
         type:Date,
         default: Date.now,
